ce34af50f8264f6a19ab5fe0f9f786444a14638a
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2025-08-11 16:56:18 -0300

    Ferramenta Front-End

---
+++ projeto/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Sara é estudante de TI e não gosta de front :)
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
